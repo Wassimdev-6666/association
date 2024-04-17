f8a70a7c2f84841757c86b4451469bdd9148c8c0
midoxchh <mohamedmehdichahboune@gmail.com>
2024-04-17 02:47:27 +0100

    edited home page | still the footer

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -14,23 +14,18 @@
         <h2>Qui Sommes-Nous</h2>
         <p>L'association des enfants, créée dans les années 1980, est une organisation dédiée à rassembler les enfants autour d'activités éducatives, récréatives et culturelles. Elle offre aux enfants un environnement sûr où ils peuvent participer à des jeux, des ateliers créatifs, des sorties éducatives et des voyages. L'objectif principal de cette association est de favoriser le développement global des enfants en stimulant leur curiosité, leur créativité et leur esprit d'équipe. En résumé, l'association des enfants offre depuis les années 1980 un cadre enrichissant où les enfants peuvent s'amuser, apprendre et grandir ensemble.</p>
         </div>
-       
-        <div class="photo de presentation">
-            <img  src="{{ asset('images/activite/slider3.jpg') }}" alt="Slide 2"/>
-        </div>
+        <img  src="{{ asset('images/activite/slider3.jpg') }}" alt="Slide 2"/>
     </section>
 
     <!-- Autres sections de contenu -->
     <section class=" section-activites">
-        <h2>Activités de l'association</h2>
+        <h1>Activités de l'association</h1>
         <div class="activité">
-            <div class="first child">
+            <div class="container-image">
             <img  src="{{ asset('images/activite/slider3.jpg') }}"/>
             <img  src="{{ asset('images/activite/slider3.jpg') }}"/>
             <img  src="{{ asset('images/activite/slider3.jpg') }}"/>
             <img  src="{{ asset('images/activite/slider3.jpg') }}"/>
-            </div>
-            <div class="secend child">
             <img  src="{{ asset('images/activite/slider3.jpg') }}"/>
             <img  src="{{ asset('images/activite/slider3.jpg') }}"/>
             <img  src="{{ asset('images/activite/slider3.jpg') }}"/>
