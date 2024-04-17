da8ca7fbb4be034a831783a4ac77b9cf44690cce
midoxchh <mohamedmehdichahboune@gmail.com>
2024-04-17 04:22:25 +0100

    fix the problem of the white space in the right

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -4,9 +4,9 @@
             autoplay: true, // Lecture automatique
             autoplaySpeed: 2000, // Vitesse de lecture automatique en millisecondes
             dots: true, // Affichage des indicateurs de pagination
-            arrows: true, // Affichage des flèches de navigation
-            prevArrow: '<button type="button" class="slick-prev">&#10094;</button>', // Flèche précédente
-            nextArrow: '<button type="button" class="slick-next">&#10095;</button>' // Flèche suivante
+            arrows: false, // Affichage des flèches de navigation
+            // prevArrow: '<button type="button" class="slick-prev"></button>', // Flèche précédente
+            // nextArrow: '<button type="button" class="slick-next"></button>' // Flèche suivante
             // Ajoutez d'autres options au besoinsdsdsdsfsf
         });
     });
